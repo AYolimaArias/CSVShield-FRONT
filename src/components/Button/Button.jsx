d8c4697652a05aef9b4daf462bf7afcc402ede52
Angelica <angelica.arias33@gmail.com>
2024-05-20 19:24:09 -0500

    [ADD] Button component added with styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,3 +1,40 @@
-export const Button = () => {
-  return <div>Button</div>;
+import styles from "../Button/module.css";
+import clsx from "clsx";
+
+const Button = ({
+  variant = "primary",
+  size = "md",
+  icon,
+  href,
+  className,
+  children,
+
+  ...delegated
+}) => {
+  const Tag = typeof href === "string" ? "a" : "button";
+
+  const variantStyle = clsx(
+    variant === "primary" && styles.default,
+    variant === "secondary" && styles.secondary
+  );
+  const sizeStyle = clsx(
+    size === "sm" && styles.sm,
+    size === "md" && styles.md,
+    size === "lg" && styles.lg
+  );
+  const styleFinal = clsx(
+    variantStyle,
+    sizeStyle,
+    "flex gap-2 items-center",
+    Tag === "a" && "underline underline-offset-4 decoration-2",
+    className
+  );
+
+  return (
+    <Tag {...delegated} href={href} className={styleFinal}>
+      {icon}
+      {children}
+    </Tag>
+  );
 };
+export default Button;
