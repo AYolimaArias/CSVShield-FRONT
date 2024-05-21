e502a9bf97b6f4918c5c26dc8f6e08dfd83062ae
Angelica <angelica.arias33@gmail.com>
2024-05-21 14:49:36 -0500

    [FIX] Fixing de error page.

--- src/errors/error-page.jsx
+++ src/errors/error-page.jsx
@@ -5,12 +5,18 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div className="flex justify-center">
-      <h1 className="mt-10">Oops!</h1>
-      <p className="mt-6">Sorry, an unexpected error has occurred.</p>
-      <p className="mt-6">
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="mt-60 flex justify-center items-center">
+      <div className="flex justify-center ml-32">
+        <p className="mt-6 mr-8 font-bold text-5xl flex justify-center">
+          <i className="mb-8">{error.statusText || error.message}</i>
+        </p>
+      </div>
+      <div>
+        <h1 className=" font-medium text-2xl">Oops!</h1>
+        <p className=" font-medium text-1xl">
+          Sorry, an unexpected error has occurred.
+        </p>
+      </div>
     </div>
   );
 }
